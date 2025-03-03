483055d5439931824f1464bdcac8e6d260e50049
G1Ismail <ismail.amw@gmail.com>
2025-03-03 13:06:07 +0100

    project structure and initial dependencies

---
+++ Books-Booking-System/src/main/java/de/BookBuch/BooksBookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package de.BookBuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksBookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksBookingSystemApiApplication.class, args);
+	}
+
+}
---
+++ Books-Booking-System/src/test/java/de/BookBuch/BooksBookingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.BookBuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksBookingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
